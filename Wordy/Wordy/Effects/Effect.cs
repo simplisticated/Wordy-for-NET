2bc6f5a434ca624f4c65b2345616a05887802ced
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-03 16:07:55 +0300

    Added Effect base class

---
+++ Wordy/Wordy/Effects/Effect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordy.Effects
+{
+    public abstract class Effect
+    {
+        public Effect()
+        {
+        }
+
+        public abstract string GetFilteredText(string sourceText);
+    }
+}

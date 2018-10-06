bb3d5099ac9b987bfebd16620c1017a218828219
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-06 12:41:01 +0300

    Added Bridge class

---
+++ Wordy/Wordy/Bridge/Bridge.cs
@@ -0,0 +1,12 @@
+﻿using Wordy.Tools;
+
+namespace Wordy.Bridge
+{
+    public class UseWordyTo
+    {
+        public static EffectManager MakeEffects(string sourceText)
+        {
+            return new EffectManager(sourceText);
+        }
+    }
+}

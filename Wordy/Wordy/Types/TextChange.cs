b5c1b7ee00e083ad68dd4d0a0b0e1a5b91f66c67
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-06 12:39:41 +0300

    Implemented TextChange class

---
+++ Wordy/Wordy/Types/TextChange.cs
@@ -0,0 +1,17 @@
+﻿using Wordy.Effects;
+
+namespace Wordy.Types
+{
+    public class TextChange
+    {
+        public Effect Effect { get; private set; }
+
+        public SubstringLocation Location { get; private set; }
+
+        public TextChange(Effect effect, SubstringLocation location)
+        {
+            this.Effect = effect;
+            this.Location = location;
+        }
+    }
+}

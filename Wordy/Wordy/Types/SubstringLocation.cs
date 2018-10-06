96d5f6d1615c180b8366d5599bc96febea9bcff7
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-06 12:39:23 +0300

    Added SubstringLocation class

---
+++ Wordy/Wordy/Types/SubstringLocation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Wordy.Types
+{
+    public class SubstringLocation
+    {
+        public int StartIndex { get; private set; }
+
+        public int EndIndex { get; private set; }
+
+        /// <summary>
+        /// Creates new substring location.
+        /// 
+        /// Start index should be less than or equal to end index. Otherwise, <see cref="WrongIndexException"/> will be thrown.
+        /// </summary>
+        /// <param name="startIndex">Start index.</param>
+        /// <param name="endIndex">End index.</param>
+        /// <exception cref="WrongIndexException"></exception>
+        public SubstringLocation(int startIndex, int endIndex)
+        {
+            if (startIndex > endIndex)
+            {
+                throw new WrongIndexException();
+            }
+
+            this.StartIndex = startIndex;
+            this.EndIndex = endIndex;
+        }
+
+        public class WrongIndexException : Exception
+        {
+        }
+    }
+}

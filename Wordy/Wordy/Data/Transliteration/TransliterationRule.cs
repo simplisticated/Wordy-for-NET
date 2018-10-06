07e04fc8cefb2174c5d34f09a336e633f294d7ac
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-06 15:23:19 +0300

    Created TransliterationRule class

---
+++ Wordy/Wordy/Data/Transliteration/TransliterationRule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordy.Data.Transliteration
+{
+    internal class TransliterationRule
+    {
+        public string Replaceable { get; private set; }
+
+        public string Replacement { get; private set; }
+
+        public TransliterationRule(string replaceable, string replacement)
+        {
+            this.Replaceable = replaceable;
+            this.Replacement = replacement;
+        }
+
+        public class Builder
+        {
+            private string Replaceable;
+
+            public Builder SetReplaceable(string replaceable)
+            {
+                this.Replaceable = replaceable;
+                return this;
+            }
+
+            private string Replacement;
+
+            public Builder SetReplacement(string replacement)
+            {
+                this.Replacement = replacement;
+                return this;
+            }
+
+            public Builder()
+            {
+            }
+
+            public TransliterationRule Build()
+            {
+                return new TransliterationRule(this.Replaceable, this.Replacement);
+            }
+        }
+    }
+}

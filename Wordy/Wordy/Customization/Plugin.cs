fb600c94229a0b2d3f4c0e9b5743fed0a8547ff6
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-06 15:33:07 +0300

    Added Plugin abstract class

---
+++ Wordy/Wordy/Customization/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordy.Customization
+{
+    public abstract class Plugin
+    {
+        public string SourceText { get; private set; }
+
+        public Plugin(string sourceText)
+        {
+            this.SourceText = sourceText;
+        }
+
+        public abstract string GetResult();
+    }
+}

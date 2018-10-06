288806c29ab45f6386a820930f14ef2f8482e8c6
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-10-06 14:51:23 +0300

    Implemented InversionEffect class

---
+++ Wordy/Wordy/Effects/InversionEffect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordy.Effects
+{
+    public class InversionEffect : Effect
+    {
+        public InversionEffect() : base()
+        {
+        }
+
+        public override string GetFilteredText(string sourceText)
+        {
+            var invertedSymbols = sourceText.ToCharArray().Reverse().ToArray();
+            return new string(invertedSymbols);
+        }
+    }
+}
